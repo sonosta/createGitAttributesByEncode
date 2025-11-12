98abcfdf9e3cd9e46db8700cd46d0cb50a5facb6
Nikita Arinchehin <narincehin@gmail.com>
2025-11-12 16:48:50 +0300

    добавлено ограничение на системные папки, автоматическое заполнение eol и working-tree-encoding

--- addAttributes.py
+++ addAttributes.py
@@ -3,30 +3,48 @@ import sys
 import chardet
 from pathlib import Path
 
+skip_dirs = ['BaseInt', 'Com', 'ComData', 'ComExt', 'Net', 'SYS', '.git']
+
 def detect_encoding(file_path):
     with open(file_path, 'rb') as f:
-        raw_data = f.read(1024)  # читаем первые 1024 байта для анализа
+        raw_data = f.read(1024)  
     result = chardet.detect(raw_data)
     return result['encoding']
+
+def check_line_endings(file_path, encoding_str):
+
+    with open(file_path, 'rb') as f:
+        content = f.read(1024)
+    if b'\r\n' in content:
+        return 'crlf'
+    elif b'\n' in content:
+        return 'lf'
+    else: 
+        return 0
 
 def recursive_encoding_check(folder):
     arr_strings = []
     for root, dirs, files in os.walk(folder):
+        print(f"dirs: {root}")
+        dirs[:] = [d for d in dirs if d not in skip_dirs]  # удаляем папки для пропуска из списка
         for file in files:
             file_path = os.path.join(root, file)
-            encoding = detect_encoding(file_path)
-
-            encoding_str = encoding if encoding is not None else ""
             extension = Path(file_path).suffix
-            short_path = str(Path(file_path).relative_to(folder)).replace('\\', '/')
-            if encoding_str != "" and extension in ['.cod', '.mtl', '.lis', '.tpl', '.bro'] :
-                if 'windows-' in encoding_str.lower() :
-                    encoding_str = encoding_str.replace('windows-', 'cp') + ' eol=crlf'
-                file_path = short_path
-                arr_strings.append(file_path  + " working-tree-encoding=" + encoding_str + "\n")
-            
-            print(f"File: {file_path}, Encoding: {encoding}, Short path: {short_path}")
-            
+            if extension in ['.cod', '.mtl', '.lis', '.bro']:
+                encoding = detect_encoding(file_path)
+                encoding_str = encoding if encoding is not None else ""
+                
+                short_path = str(Path(file_path).relative_to(folder)).replace('\\', '/')
+                if encoding_str != "" :
+                    if 'windows-' in encoding_str.lower() :
+                        encoding_str = encoding_str.lower().replace('windows-', 'cp') 
+                        eol = check_line_endings(file_path, encoding_str)
+                        if eol != 0:
+                            encoding_str = encoding_str + ' eol='+ eol
+                        file_path = short_path
+                        arr_strings.append('"'+ file_path + '"' + " working-tree-encoding=" + encoding_str + "\n")
+                        # print(f"File: {file_path}, Encoding: {encoding}, Short path: {short_path}, EOL: {eol}")
+
     with open(".gitattributes", "w", encoding="utf-8") as f:
         f.writelines(arr_strings)
 
